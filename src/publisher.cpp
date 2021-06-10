60c1210079fe7625f58e40d77057557288993b84
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-10 11:59:12 -0600

    Stop using Pythonic private naming scheme

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -11,19 +11,19 @@ class MultiCloudPublisher : public rclcpp::Node
 public:
 	MultiCloudPublisher() : Node("multi_cloud")
 	{
-		_publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("multi_cloud", 1);
+		publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("multi_cloud", 1);
 		this->declare_parameter<std::vector<std::string>>("point_cloud_topics", {});
 		this->declare_parameter<std::string>("base_frame", "world");
-		this->get_parameter("point_cloud_topics", _point_cloud_topics);
-		this->get_parameter("base_frame", _base_frame);
+		this->get_parameter("point_cloud_topics", point_cloud_topics);
+		this->get_parameter("base_frame", base_frame);
 
-		_tf_buffer = std::make_shared<tf2_ros::Buffer>(this->get_clock());
-		_tf_listener = std::make_shared<tf2_ros::TransformListener>(*_tf_buffer, true);
+		tf_buffer = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+		tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer, true);
 
-		for (auto point_cloud_topic : this->_point_cloud_topics)
+		for (auto point_cloud_topic : this->point_cloud_topics)
 		{
 			RCLCPP_INFO(this->get_logger(), "Adding subscription for point cloud at path: %s", point_cloud_topic.c_str());
-			_subscriptions.push_back(this->create_subscription<sensor_msgs::msg::PointCloud2>(
+			subscriptions.push_back(this->create_subscription<sensor_msgs::msg::PointCloud2>(
 				point_cloud_topic, rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data)),
 				std::bind(&MultiCloudPublisher::point_cloud_callback, this, std::placeholders::_1)));
 		}
@@ -36,7 +36,7 @@ private:
 		std::string frame_id = msg->header.frame_id;
 		try
 		{
-			transform_stamped_msg = _tf_buffer->lookupTransform("world", frame_id, rclcpp::Time(0));
+			transform_stamped_msg = tf_buffer->lookupTransform("world", frame_id, rclcpp::Time(0));
 		}
 		catch (tf2::TransformException &ex)
 		{
@@ -48,19 +48,19 @@ private:
 		pcl::moveFromROSMsg(*msg, *pcl_cloud);
 		pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_transformed_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
 		pcl::transformPointCloud(*pcl_cloud, *pcl_transformed_cloud, tf2::transformToEigen(transform_stamped_msg).matrix());
-		_point_clouds[frame_id] = pcl_transformed_cloud;
+		point_clouds[frame_id] = pcl_transformed_cloud;
 
 		publish_points();
 	}
 	void publish_points()
 	{
-		if (_point_clouds.empty())
+		if (point_clouds.empty())
 		{
 			return;
 		}
 
 		pcl::PointCloud<pcl::PointXYZRGB> combined_pcl_cloud;
-		for (const auto &points_pair : _point_clouds)
+		for (const auto &points_pair : point_clouds)
 		{
 			combined_pcl_cloud += *points_pair.second;
 		}
@@ -71,15 +71,15 @@ private:
 		combined_cloud.header.frame_id = "world";
 		combined_cloud.header.stamp = now();
 
-		_publisher->publish(combined_cloud);
+		publisher->publish(combined_cloud);
 	}
-	std::vector<rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr> _subscriptions;
-	rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr _publisher;
-	std::vector<std::string> _point_cloud_topics;
-	std::shared_ptr<tf2_ros::Buffer> _tf_buffer;
-	std::shared_ptr<tf2_ros::TransformListener> _tf_listener;
-	std::string _base_frame;
-	std::map<std::string, pcl::PointCloud<pcl::PointXYZRGB>::Ptr> _point_clouds;
+	std::vector<rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr> subscriptions;
+	rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher;
+	std::vector<std::string> point_cloud_topics;
+	std::shared_ptr<tf2_ros::Buffer> tf_buffer;
+	std::shared_ptr<tf2_ros::TransformListener> tf_listener;
+	std::string base_frame;
+	std::map<std::string, pcl::PointCloud<pcl::PointXYZRGB>::Ptr> point_clouds;
 };
 
 int main(int argc, char *argv[])
